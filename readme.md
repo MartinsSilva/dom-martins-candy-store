182c9cb1cae43107dcd5757a9bafc417920efe52
MartinsSilva <gouveiamartins6@gmail.com>
2020-09-19 21:51:33 +0100

    primeiro commit

---
+++ readme.md
@@ -0,0 +1,18 @@
+# Dom Martins - Loja de doces 🥞🥞
+
+"Dom Martins" é uma pequena aplicação que está sendo construído com  [Vue.js](https://vuejs.org/v2/guide/) com a finalidade de treinar os meus conhecimentos em Vue.js.
+
+### Screenshots
+
+![one](resources/screen/home.png)
+![two](resources/screen/list.png)
+
+## Instalação
+
+Para rodar essa aplicação é simples. É só fazeres o clone deste repositório e abrires apartir do seu localhost.
+
+## Contribuindo
+
+Esse é um projeto livre e você pode contribuir com qualquer coisa: Melhorando o readme, aplicando novas funcionalidades e etc....
+
+### Obs: Faça os commits e os testes antes do pull request.
---
+++ resources/js/app.js
@@ -0,0 +1,43 @@
+var app = new Vue({
+	el: '#root',
+	data: {
+		products: [
+			{
+				name: 'Ice Cream',
+				price: '500',
+				toImage: 'sweets-1.jpeg',
+				description: "lorem lorem lorem"
+			},
+			{
+				name: 'KitKat',
+				price: '2500',
+				toImage: 'sweets-2.jpeg',
+				description: "lorem lorem lorem"
+			},
+			{
+				name: 'Lollipop',
+				price: '2500',
+				toImage: 'doughnut-3.jpeg',
+				description: "lorem lorem lorem"
+			},
+			{
+				name: 'Marshmallow',
+				price: '2500',
+				toImage: 'cake-3.jpeg',
+				description: "lorem lorem lorem"
+			},
+			{
+				name: 'Lollipop',
+				price: '2500',
+				toImage: 'cake-1.jpeg',
+				description: "lorem lorem lorem"
+			},
+			{
+				name: 'Oreo',
+				price: '2500',
+				toImage: 'cupcake-1.jpeg',
+				description: "lorem lorem lorem"
+			}			
+		]
+	}
+})
